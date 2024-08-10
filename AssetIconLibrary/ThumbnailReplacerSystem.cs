fc8c6503b9189851dacc4c568c1ba704bb77e219
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-08-10 17:38:41 +0200

    comment

--- AssetIconLibrary/ThumbnailReplacerSystem.cs
+++ AssetIconLibrary/ThumbnailReplacerSystem.cs
@@ -56,7 +56,7 @@ namespace AssetIconLibrary {
                     if (Mod.Settings.OverwriteIcons
                         // almost none of the uiObjects have an icon
                         // imageSystem does the trick
-                        // this causes ExtraAssets to not show some icons
+                        // this causes that some Extra...-Icons are not shown
                         // || String.IsNullOrWhiteSpace(uIObject.m_Icon)
                         ) {
                         uIObject.m_Icon = newIcon;
